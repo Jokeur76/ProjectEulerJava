877d00e79ebc4076c208425bf9c3c19dee4cae7a
LarchMickael <mickael.larcheveque@gmail.com>
2015-07-04 08:10:46 +0200

    project euler problème 4

--- ProjectEuler/src/JavaProjectEulerLib.java
+++ ProjectEuler/src/JavaProjectEulerLib.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-import java.util.Vector;
 
 
 
 public class JavaProjectEulerLib {
 
-
+	public static boolean isPalindromic(String str){
+		boolean isPala = false;
+		StringBuilder strB = new StringBuilder(str);
+		String strRev = strB.reverse().toString();
+		if(strRev.equals(str))isPala = true;
+		
+		return isPala;
+	}
 	
 	public static boolean isPrime(double nbr){
 		boolean test = true;
--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -4,25 +4,14 @@ public class Main extends JavaProjectEulerLib {
  
  
         public static void main(String[] args) {
-                long rez = 0;
-                long checkedValue = 600851475143l;
-                long divisor = 2;
-                while ((divisor * divisor) <= checkedValue) {
-                        if ((checkedValue % divisor) == 0) {
-                                rez = divisor;
-                                System.out.println("diviseur " +divisor);
-                                checkedValue /= divisor;
-                                System.out.println("reste : "+checkedValue);
-                        }
-                        else {
-                                divisor++;
-                        }
-                }
-               
-                if (checkedValue != 1) {
-                        rez = checkedValue;
-                }
-               
-                System.out.println(rez);
+        	int result = 0;
+        	for(int i = 999; i >= 100; i--){
+        		for(int j = 999; j >= 100; j--){
+        			if(i*j > result && isPalindromic(String.valueOf(i*j))){
+        				result = i*j;
+        			}
+        		}
+        	}
+        	System.out.println(result);
         }
 }
